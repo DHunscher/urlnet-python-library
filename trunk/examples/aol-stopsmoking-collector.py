8de82d3fc464686a751aa6991a263b0943445266
dalehunscher <dalehunscher@ca67e0b6-c54b-11dd-92e3-e3085bfe42ca>
2009-10-29 13:04:09 +0000

    Changed logfiles ending in log to end in log.txt to avoid invoking the Mac OS X Console.

--- trunk/examples/aol-stopsmoking-collector.py
+++ trunk/examples/aol-stopsmoking-collector.py
@@ -68,7 +68,7 @@ def main(which):
         myLog = Log('main')
         urlnet.log.logging=True
         #urlnet.log.trace=True
-        urlnet.log.altfd=open('aoltree.log','w')
+        urlnet.log.altfd=open('aoltree.log.txt','w')
     except Exception,e:
         myLog.Write(str(e)+'\n')
         goAhead = False
